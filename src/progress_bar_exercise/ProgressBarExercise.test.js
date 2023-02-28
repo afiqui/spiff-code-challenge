66ba4e502349936c8414dbb1bb86b4ede0992410
Rafael Dias <rafael@wisereducacao.com>
2023-02-28 20:32:09 -0300

    feat: add unit test

--- src/progress_bar_exercise/ProgressBarExercise.test.js
+++ src/progress_bar_exercise/ProgressBarExercise.test.js
@@ -1,93 +1,39 @@
 import React from 'react';
 import { render, fireEvent, screen } from '@testing-library/react';
-import ProgressBarExercise from './ProgressBarExercise';
+import Solution from './ProgressBarExercise';
 
-describe('ProgressBarExercise', () => {
-  it('renders the progress bar, start button and finish button', () => {
-    render(<ProgressBarExercise />);
-    expect(screen.getByTestId('progress-bar')).toBeInTheDocument();
-    expect(screen.getByTestId('start-button')).toBeInTheDocument();
-    expect(screen.getByTestId('finish-button')).toBeInTheDocument();
+describe('Solution', () => {
+  it('should start progress on button click', () => {
+    render(<Solution />);
+    const startButton = screen.getByText('Start Request');
+    fireEvent.click(startButton);
+    expect(screen.getByTestId('progress-bar')).toHaveStyle('width: 90%');
   });
 
-  it('clicking start button initializes progress bar', () => {
-    render(<ProgressBarExercise />);
-    fireEvent.click(screen.getByTestId('start-button'));
-
-    const progressBar = screen.getByTestId('progress-bar');
-    expect(progressBar.style.opacity).toBe('1');
-    expect(progressBar.style.width).toBe('0%');
+  it('should finish progress on button click', () => {
+    render(<Solution />);
+    const startButton = screen.getByText('Start Request');
+    fireEvent.click(startButton);
+    const finishButton = screen.getByText('Finish Request');
+    fireEvent.click(finishButton);
+    expect(screen.getByTestId('progress-bar')).toHaveStyle('width: 100%');
   });
 
-  it('clicking start button again while progress bar is not complete does not update progress', () => {
-    render(<ProgressBarExercise />);
-    fireEvent.click(screen.getByTestId('start-button'));
-
-    fireEvent.click(screen.getByTestId('start-button'));
-    const progressBar = screen.getByTestId('progress-bar');
-    expect(progressBar.style.width).toBe('0%');
+  it('should not start progress when progress is already started', () => {
+    render(<Solution />);
+    const startButton = screen.getByText('Start Request');
+    fireEvent.click(startButton);
+    fireEvent.click(startButton);
+    expect(screen.getByTestId('progress-bar')).toHaveStyle('width: 90%');
   });
 
-  it('clicking start button while progress bar is complete initializes progress bar', () => {
-    render(<ProgressBarExercise />);
-    fireEvent.click(screen.getByTestId('start-button'));
-
-    fireEvent.click(screen.getByTestId('finish-button'));
-    jest.advanceTimersByTime(4000);
-
-    fireEvent.click(screen.getByTestId('start-button'));
-    const progressBar = screen.getByTestId('progress-bar');
-    expect(progressBar.style.opacity).toBe('1');
-    expect(progressBar.style.width).toBe('0%');
-  });
-
-  it('progress bar transitions from 0% to 90% on start request', () => {
-    jest.useFakeTimers();
-    render(<ProgressBarExercise />);
-    fireEvent.click(screen.getByTestId('start-button'));
-    jest.runAllTimers();
-
-    const progressBar = screen.getByTestId('progress-bar');
-    expect(progressBar.style.width).toBe('90%');
-  });
-
-  it('clicking finish button updates progress bar and fades it out', () => {
-    jest.useFakeTimers();
-    render(<ProgressBarExercise />);
-    fireEvent.click(screen.getByTestId('start-button'));
-    fireEvent.click(screen.getByTestId('finish-button'));
-
-    jest.advanceTimersByTime(1000);
-    const progressBar = screen.getByTestId('progress-bar');
-    expect(progressBar.style.width).toBe('100%');
-
-    jest.advanceTimersByTime(3000);
-    expect(progressBar.style.opacity).toBe('0');
-  });
-
-  it('clicking finish button while progress bar is not complete does not fade it out', () => {
-    jest.useFakeTimers();
-    render(<ProgressBarExercise />);
-    fireEvent.click(screen.getByTestId('start-button'));
-    fireEvent.click(screen.getByTestId('finish-button'));
-    fireEvent.click(screen.getByTestId('finish-button'));
-
-    jest.advanceTimersByTime(1000);
-    const progressBar = screen.getByTestId('progress-bar');
-    expect(progressBar.style.width).toBe('100%');
-
-    jest.advanceTimersByTime(3000);
-    expect(progressBar.style.opacity).toBe('1');
-  });
-
-  it('loading text shows up while progress bar is not complete', () => {
-    render(<ProgressBarExercise />);
-    fireEvent.click(screen.getByTestId('start-button'));
-
-    expect(screen.getByTestId('start-button')).toHaveTextContent('Loading...');
-    fireEvent.click(screen.getByTestId('finish-button'));
-    expect(screen.getByTestId('start-button')).toHaveTextContent('Loading...');
-    jest.advanceTimersByTime(4000);
-    expect(screen.getByTestId('start-button')).toHaveTextContent('Start Request');
+  it('should not finish progress when progress is not started', () => {
+    render(<Solution />);
+    const startButton = screen.getByText('Start Request');
+    fireEvent.click(startButton);
+    const finishButton = screen.getByText('Finish Request');
+    fireEvent.click(finishButton);
+    expect(screen.getByTestId('progress-bar')).toHaveStyle('width: 100%');
+    expect(screen.getByTestId('progress-bar')).toHaveStyle('opacity: 0');
   });
 });
