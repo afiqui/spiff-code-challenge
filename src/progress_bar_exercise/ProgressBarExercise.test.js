b54f02f45d21716dd34112dff2825c205d067b9e
Rafael Dias <rafael@wisereducacao.com>
2023-03-02 15:48:18 -0300

    feat: add unit test for v2

--- src/progress_bar_exercise/ProgressBarExercise.test.js
+++ src/progress_bar_exercise/ProgressBarExercise.test.js
@@ -36,4 +36,24 @@ describe('Solution', () => {
     expect(screen.getByTestId('progress-bar')).toHaveStyle('width: 100%');
     expect(screen.getByTestId('progress-bar')).toHaveStyle('opacity: 0');
   });
+
+  it("should stop when have a breakpoint", () => {
+    render(<Solution />);
+
+    const startButton = screen.getByText("Start Request");
+    const breakPointCheckBox = screen.getByText("Use Breakpoints");
+    fireEvent.click(breakPointCheckBox);
+
+    const breakPointInput = screen.getByPlaceholderText("0, 20, 40, 60, 90...");   
+
+    fireEvent.change(breakPointInput, { target: { value: "10" } });
+
+    expect(breakPointInput.value).toBe("10");
+
+    
+    fireEvent.click(startButton);
+
+    expect(screen.getByTestId("progress-bar")).toBeInTheDocument();
+    expect(screen.getByTestId('progress-bar')).toHaveStyle('width: 10%');
+  });
 });
